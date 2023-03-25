8aa7ac93658762f33029cd88e6563569d30b62af
nathan-assefa <nnathanassefa@gmail.com>
2023-03-25 07:00:28 -0700

    0x0B-ssh

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,8 @@
+file { '/etc/ssh/ssh_config':
+  ensure  => 'present',
+  content => '
+    SendEnv LANG LC_*
+    HashKnownHosts yes
+    GSSAPIAuthentication yes
+    IdentityFile ~/.ssh/school
+    PasswordAuthentication no ' }
