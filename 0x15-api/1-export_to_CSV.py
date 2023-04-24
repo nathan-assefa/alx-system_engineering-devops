99ab43e5c0ed39450a982acbc772055033b67a23
nathan-assefa <nnathanassefa@gmail.com>
2023-04-24 08:26:50 -0700

    API

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -2,12 +2,9 @@
 """
 Exports to-do list information for a given employee ID to CSV format.
 """
-
-
 import csv
 import requests
 import sys
-
 
 if __name__ == "__main__":
     user_id = sys.argv[1]
