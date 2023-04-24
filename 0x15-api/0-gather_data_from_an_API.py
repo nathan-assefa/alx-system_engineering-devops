e6597fc7147c71b1cc576e290d65cb99fc39b671
nathan-assefa <nnathanassefa@gmail.com>
2023-04-24 08:02:04 -0700

    API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -9,34 +9,15 @@ endpoint
 
 
 import requests
-from sys import argv
+import sys
 
 
 if __name__ == '__main__':
-    """ fetching some data about user using third party api """
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
 
-    prim_url = 'https://jsonplaceholder.typicode.com'
-    name_endpoint = '/users/{}'
-
-    # here we find the name of the user from /users/id endpoint
-    name_res = requests.get(prim_url + name_endpoint.format(argv[1]))
-    _dict1 = name_res.json()
-    name = _dict1['name']
-
-    # here we find the python object that represents the todos list
-    todos_res = requests.get(prim_url + '/todos', params={'userId': argv[1]})
-    todos = todos_res.json()
-
-    # let us count the completed task by user using the sum function
-    completed_tasks = sum([1 for todo in todos if todo['completed']])
-
-    # count the sum of the completed and non-completed taks
-    total_completed_tasks = len(todos)
-
-    print('Employee {} is done with tasks({}/{})'.format(
-        name, completed_tasks, total_completed_tasks))
-
-    # print the title of completed task
-    for task in todos:
-        if task['completed']:
-            print('\t {}'.format(task['title']))
+    completed = [t.get("title") for t in todos if t.get("completed") is True]
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get("name"), len(completed), len(todos)))
+    [print("\t {}".format(c)) for c in completed]
--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -11,8 +11,17 @@ if __name__ == "__main__":
     username = user.get("username")
     todos = requests.get(url + "todos", params={"userId": user_id}).json()
 
-    with open("{}.csv".format(user_id), "w", newline="") as csvfile:
-        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-        [writer.writerow(
-            [user_id, username, t.get("completed"), t.get("title")]
-         ) for t in todos]
+    with open(f"{user_id}.csv", "w") as csvfile:
+        fieldnames = ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"]
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+
+    for todo in todos:
+        writer.writerow(
+            {
+                "USER_ID": todo["userId"],
+                "USERNAME": "",  # Add the employee username here
+                "TASK_COMPLETED_STATUS": str(todo["completed"]),
+                "TASK_TITLE": todo["title"],
+            }
+        )
