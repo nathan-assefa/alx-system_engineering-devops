0f429132ab24b6fd4ad06d6cba4f5e3122f37d42
nathan-assefa <nnathanassefa@gmail.com>
2023-04-24 03:54:37 -0700

    API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,37 +6,35 @@ endpoint """
 
 
 import requests
-import sys
+from sys import argv
 
+if __name__ == '__main__':
+    """ fetching some data about user using third party api """
 
-if __name__ == __main__:
-    """ talking to jsonplaceholder api """
-    # The base URL for the API
-    base_url = 'https://jsonplaceholder.typicode.com'
+    prim_url = 'https://jsonplaceholder.typicode.com'
+    name_endpoint = '/users/{}'
+    tode_endpoint = '/todos?userId={}'
 
-    # The endpoint for retrieving employee information
-    employee_endpoint = '/users/{}'
+    # here we find the name of the user from /users/id endpoint
+    name_res = requests.get(prim_url + name_endpoint.format(argv[1]))
+    _dict1 = name_res.json()
+    name = _dict1['name']
 
-    # The endpoint for retrieving TODO list information
-    todo_endpoint = '/todos?userId={}'
+    # here we find the python object that represents the todos list
+    todos_res = requests.get(prim_url + tode_endpoint.format(argv[1]))
+    todos = todos_res.json()
 
-    # The employee ID is provided as a command line argument
-    employee_id = sys.argv[1]
+    # let us count the completed task by user using the sum function
+    completed_tasks = sum([1 for todo in todos if todo['completed']])
 
-    # Retrieve employee information
-    employee_response = requests.get(base_url + employee_endpoint.format(employee_id))
-    employee = employee_response.json()
+    # count the sum of the completed and non-completed taks
+    total_completed_tasks = len(todos)
 
-    # Retrieve TODO list information
-    todo_response = requests.get(base_url + todo_endpoint.format(employee_id))
-    todos = todo_response.json()
+    print('Employee {} is done with tasks({}/{})'.format(
+        name, completed_tasks, total_completed_tasks))
 
-    # Count the number of completed and total tasks
-    num_completed_tasks = sum([1 for todo in todos if todo['completed']])
-    num_total_tasks = len(todos)
-
-    # Display the employee's TODO list progress
-    print("Employee {} is done with tasks({}/{}):".format(employee['name'], num_completed_tasks, num_total_tasks))
-    for todo in todos:
-        if todo['completed']:
-            print("\t {} {}".format('\t', todo['title']))
+    # print the title of completed task
+    for task in todos:
+        if task['completed']:
+            print('\t ', end='')
+            print(task['title'])
