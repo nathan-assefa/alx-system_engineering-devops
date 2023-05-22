ae0e08412798719e6122c38a214ac7a79e104cf2
nathan-assefa <aasamnew@gmail.com>
2023-05-22 10:42:14 +0000

    API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -12,19 +12,19 @@ import requests
 from sys import argv
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
-    
-    # find the user name from the 'users' end point, and parse it 
-    user = requests.get(url + 'users/{}'.format(argv[1])).json()
-    user_name = user['name']
+
+    # find the user name from the 'users' end point, and parse it
+    user = requests.get(url + "users/{}".format(argv[1])).json()
+    user_name = user["name"]
     # Notice: the json() method is part of the response object
 
     # find the list of todos and parse it into python list
-    todos = requests.get(url + 'todos?userId={}'.format(argv[1])).json()
+    todos = requests.get(url + "todos?userId={}".format(argv[1])).json()
 
     # find number of tasks done by the user
-    tasks_done = [task_done for task_done in todos if task_done['completed']]
+    tasks_done = [task_done for task_done in todos if task_done["completed"]]
 
     len_tasks_done = len(tasks_done)
 
@@ -32,10 +32,11 @@ if __name__ == '__main__':
 
     total_task = len(todos)
 
-    print("Employee {} is done with tasks({}/{}):".format(
-        user_name, len_tasks_done, total_task
-        ))
- 
+    print(
+        "Employee {} is done with tasks({}/{}):".format(
+            user_name, len_tasks_done, total_task
+        )
+    )
+
     # printing each completed tasks using list comperehesion
-    [print('\t {}'.format(task.get('title'))) for task in tasks_done]
-    
+    [print("\t {}".format(task.get("title"))) for task in tasks_done]
