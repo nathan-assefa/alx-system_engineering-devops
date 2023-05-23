8ffed9ec2a33a4f5c8308172ebf9cdc7101e4e8c
nathan-assefa <aasamnew@gmail.com>
2023-05-23 06:32:28 +0000

    API

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
+""" Using task 3 Python script to export data in the JSON format. """
 
-"""
-This script talks to jsonplaceholder api to fetch
-data about a user that includes the name of the user from
-/users api endpoint and todo lists from todos?userId={} api
-endpoint
-"""
 
 import json
 import requests
@@ -13,23 +8,14 @@ import requests
 
 if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
-
-    # find the user name from the 'users' end point, and parse it
     users = requests.get(url + "users").json()
 
-    # find the list of todos and parse it into python list
-    todos = requests.get(url + "todos?").json()
-
-    emp_data = {
-        user.get("id"): [
-            {
-                "username": user.get("username"),
-                "task": task.get("title"),
-                "completed": task.get("completed"),
-            }
-            for task in todos
-        ]
-        for user in users
-    }
     with open("todo_all_employees.json", "w") as jsonfile:
-        json.dump(emp_data, jsonfile)
+        json.dump({
+            u.get("id"): [{
+                "task": t.get("title"),
+                "completed": t.get("completed"),
+                "username": u.get("username")
+            } for t in requests.get(url + "todos",
+                                    params={"userId": u.get("id")}).json()]
+            for u in users}, jsonfile)
