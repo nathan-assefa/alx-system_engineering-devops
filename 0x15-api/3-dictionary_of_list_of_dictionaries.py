353e52f972e8a7aba254c934d5e7e2e1cb0edc68
nathan-assefa <aasamnew@gmail.com>
2023-05-23 06:31:44 +0000

    API

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -23,9 +23,9 @@ if __name__ == "__main__":
     emp_data = {
         user.get("id"): [
             {
+                "username": user.get("username"),
                 "task": task.get("title"),
                 "completed": task.get("completed"),
-                "username": user.get("username")
             }
             for task in todos
         ]
