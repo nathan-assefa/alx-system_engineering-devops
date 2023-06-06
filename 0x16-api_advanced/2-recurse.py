b5eb2cfac61e6305ac310d4a771603bcadfa220a
= <=>
2023-06-06 04:20:31 -0700

    API advanced

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -9,12 +9,11 @@ import requests
 def recurse(subreddit, hot_list=[], after=""):
     """Returns a list of titles of all hot posts on a given subreddit."""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
+    headers = {"User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"}
     params = {"after": after, "limit": 100} if after else {"limit": 100}
-    response = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
+    response = requests.get(
+            url, headers=headers, params=params, allow_redirects=False
+            )
     if response.status_code == 404:
         return None
 
@@ -26,3 +25,22 @@ def recurse(subreddit, hot_list=[], after=""):
     if after:
         return recurse(subreddit, hot_list, after)
     return hot_list
+
+
+""" The after parameter is used to indicate to the Reddit API which specific
+page of results you want to retrieve. It acts as a marker or reference point
+for the API to know from where to continue fetching the next set of results.
+
+Initially, when you make the first request to the API, you don't have an
+after value because you haven't fetched any pages yet. The API will respond
+with the first set of results along with an after value. This after value
+corresponds to the last item of the current page of results.
+
+In subsequent recursive calls, you include the after value as a parameter to
+the recurse() function. This allows you to pass the after value from the
+previous response to the next API request. By including the after value in
+the request parameters, you're telling the API to retrieve the next page of
+results starting from that particular item.
+
+So, in each recursive call, the after value is passed as a
+parameter to the function """
--- 0x16-api_advanced/222.py
+++ 0x16-api_advanced/222.py
@@ -6,31 +6,41 @@ list containing the titles of all hot articles for a given subreddit  """
 import requests
 
 
-def recurse(subreddit, hot_list=[], after=None):
-    """ Recursive funtion that queries the Reddit API """
-
-    url = "https://www.reddit.com/r/{}/hot.json".format('subreddit')
-    headers = {'User-Agent': 'MyRedditScript/1.0 (Linux; Python)'}
-    params = {"limit": 100, "after": after} if after else {"limit": 100}
-
-    try:
-        response = requests.get(
-                url, headers=headers, params=params, allow_redirects=False
-                )
-        data = response.json()
-
-        if response.status_code == 200:
-            posts = data.get("data", {}).get("children", [])
-            for post in posts:
-                title = post.get("data", {}).get("title")
-                hot_list.append(title)
-
-            after = data.get("data", {}).get("after")
-            if after:
-                return recurse(subreddit, hot_list, after=after)
-            else:
-                return hot_list
-        else:
-            return None
-    except requests.exceptions.RequestException as e:
+def recurse(subreddit, hot_list=[], after=""):
+    """Returns a list of titles of all hot posts on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    params = {"after": after, "limit": 100} if after else {"limit": 100}
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code == 404:
         return None
+
+    results = response.json().get("data")
+    after = results.get("after")
+    for data in results.get("children"):
+        hot_list.append(data.get("data").get("title"))
+
+    if after:
+        return recurse(subreddit, hot_list, after)
+    return hot_list
+
+""" The after parameter is used to indicate to the Reddit API which specific
+page of results you want to retrieve. It acts as a marker or reference point
+for the API to know from where to continue fetching the next set of results.
+
+Initially, when you make the first request to the API, you don't have an
+after value because you haven't fetched any pages yet. The API will respond
+with the first set of results along with an after value. This after value
+corresponds to the last item of the current page of results.
+
+In subsequent recursive calls, you include the after value as a parameter to
+the recurse() function. This allows you to pass the after value from the
+previous response to the next API request. By including the after value in
+the request parameters, you're telling the API to retrieve the next page of
+results starting from that particular item.
+
+So, in each recursive call, the after value is passed as a 
+parameter to the function """
