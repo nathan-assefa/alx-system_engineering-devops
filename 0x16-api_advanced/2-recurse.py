0f433c93bea0f3bc664e8e92994deb4c774b578d
= <=>
2023-06-06 03:13:40 -0700

    API advanced

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/usr/bin/python3
 """ Writing a recursive function that queries the Reddit API and returns a
 list containing the titles of all hot articles for a given subreddit  """
 
