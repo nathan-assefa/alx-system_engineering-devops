a0da3329ba7d4ae86c096e81f05528624bc9c185
nathan-assefa <aasamnew@gmail.com>
2023-06-15 09:04:20 +0000

    0x1B-web_stack_debugging_4

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,6 @@
+# puppet script to enhance the number of file discriptors for nginx service
+
+exec {'change_userlimit':
+        command => '/usr/bin/env sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
+}
+-> exec {'/usr/bin/env service nginx restart':
